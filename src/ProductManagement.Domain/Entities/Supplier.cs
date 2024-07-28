4339c22018fa35ab29048741d6d6e984e50bad9a
Renan-Santana <renanblog15@gmail.com>
2024-07-28 00:21:48 -0300

    refactor: Adicionar coleção de produtos para fornecedor

--- src/ProductManagement.Domain/Entities/Supplier.cs
+++ src/ProductManagement.Domain/Entities/Supplier.cs
@@ -1,9 +1,12 @@
-﻿namespace ProductManagement.Domain.Entities
+﻿using System.Collections.Generic;
+
+namespace ProductManagement.Domain.Entities
 {
     public class Supplier : EntityBase<short>
     {
         public string Description { get; set; }
         public string Cnpj { get; set; }
+        public ICollection<Product> Products { get; set; } = new List<Product>();
 
     }
 }
