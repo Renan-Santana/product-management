5dd76a7ef9803a0f165288583704a6b9c34c2d61
Renan-Santana <renanblog15@gmail.com>
2024-07-28 22:54:18 -0300

    refactor: Refatorar método OnModelCreating

--- src/ProductManagement.Infrastructure/Data/ProductManagementContext.cs
+++ src/ProductManagement.Infrastructure/Data/ProductManagementContext.cs
@@ -8,14 +8,66 @@ namespace ProductManagement.Infrastructure.Data
         public ProductManagementContext(DbContextOptions<ProductManagementContext> options) : base(options) { }
 
         public DbSet<Product> Products { get; set; }
-        public DbSet<Supplier> Suppliers{ get; set; }
+        public DbSet<Supplier> Suppliers { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            modelBuilder.Entity<Product>()
-                .HasOne(e => e.Supplier)
-                .WithMany(e => e.Products)
-                .HasForeignKey(e => e.SupplierId);
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Product>(entity =>
+            {
+                entity.ToTable("product");
+
+                entity.HasKey(e => e.Id);
+
+                entity.Property(e => e.Id)
+                    .HasColumnName("id");
+
+                entity.Property(e => e.Description)
+                    .IsRequired()
+                    .HasMaxLength(100)
+                    .HasColumnName("description");
+
+                entity.Property(e => e.Situation)
+                    .IsRequired()
+                    .HasColumnName("situation");
+
+                entity.Property(e => e.ManufacturingDate)
+                    .HasColumnName("manufacturing_date");
+
+                entity.Property(e => e.ExpirationDate)
+                    .HasColumnName("expiration_date");
+
+                entity.Property(e => e.SupplierId)
+                    .HasColumnName("supplier_id");
+
+                entity.HasOne(e => e.Supplier)
+                    .WithMany(s => s.Products)
+                    .HasForeignKey(e => e.SupplierId);
+            });
+
+            modelBuilder.Entity<Supplier>(entity =>
+            {
+                entity.ToTable("supplier");
+
+                entity.HasKey(e => e.Id);
+
+                entity.Property(e => e.Id)
+                    .HasColumnName("id");
+
+                entity.Property(e => e.Description)
+                    .IsRequired()
+                    .HasMaxLength(100)
+                    .HasColumnName("description");
+
+                entity.Property(e => e.Cnpj)
+                    .IsRequired()
+                    .HasMaxLength(14)
+                    .HasColumnName("cnpj");
+
+                entity.HasIndex(e => e.Cnpj)
+                    .IsUnique();
+            });
         }
     }
 }
