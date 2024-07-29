04432f042451ef4aa44893556ac02fab33b3e4c9
Renan-Santana <renanblog15@gmail.com>
2024-07-29 19:13:38 -0300

    test: Criar mock para testes

---
+++ src/ProductManagement.Test/ProductMockFactory.cs
@@ -0,0 +1,34 @@
+﻿using FluentValidation.Results;
+using ProductManagement.Application.DTOs.Request;
+using ProductManagement.Application.DTOs.Response;
+using ProductManagement.Domain.Entities;
+using System.Collections.Generic;
+using System.Linq;
+
+
+namespace ProductManagement.Test
+{
+    internal static class ProductMockFactory
+    {
+        internal static int productId { get; set; } = 1;
+        internal static Product emptyProduct { get; set; } = new Product();
+        internal static ValidationResult emptyValidationResult { get; set; } = new ValidationResult();
+        internal static ProductRequestDto emptyProductRequestDto { get; set; } = new ProductRequestDto();
+        internal static ProductResponseDto emptyProductResponseDto { get; set; } = new ProductResponseDto();
+        internal static ProductFilterRequestDto emptyProductFilterRequestDto { get; set; } = new ProductFilterRequestDto { PageNumber = 1, PageSize = 10 };
+        internal static IEnumerable<Product> emptyProductList { get; set; } = Enumerable.Empty<Product>();
+        internal static IEnumerable<Product> validProductList { get; set; } = Enumerable.Repeat(emptyProduct, 1);
+        internal static List<ProductResponseDto> validProductResponseDtoList { get; set; } = new List<ProductResponseDto> { emptyProductResponseDto };
+
+
+        internal static ValidationResult GetValidationResult()
+        {
+            var validationResult = new ValidationResult(new List<ValidationFailure>
+            {
+                new ValidationFailure("Field", "Validation failed")
+            });
+
+            return validationResult;
+        }
+    }
+}
